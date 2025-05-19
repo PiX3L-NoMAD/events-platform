463d400232606286ecc4d53029507e1e9e506931
Janilee <janileesvardstal@gmail.com>
2025-05-19 18:39:38 +0100

    adds Header component and Filter context and styling

--- SE/events-platform/client/src/App.tsx
+++ SE/events-platform/client/src/App.tsx
@@ -5,29 +5,38 @@ import {
 } from 'react-router-dom';
 import EventListPage from './pages/EventListPage';
 import EventDetailPage from './pages/EventDetailPage';
+import Header from './components/Header';
+import { FilterProvider } from './contexts/FilterContext';
 
 export default function App() {
   return (
-    <div className='min-h-screen bg-gray-50'>
-      <Routes>
-        <Route
-          path='/events'
-          element={<EventListPage />}
-        />
-        <Route
-          path='/events/:id'
-          element={<EventDetailPage />}
-        />
-        <Route
-          path='*'
-          element={
-            <Navigate
-              to='/events'
-              replace
+    <FilterProvider>
+      <div className='min-h-screen bg-gray-50'>
+        <Header />
+
+        {/* add top padding equal to header height */}
+        <div className='pt-14'>
+          <Routes>
+            <Route
+              path='/events'
+              element={<EventListPage />}
             />
-          }
-        />
-      </Routes>
-    </div>
+            <Route
+              path='/events/:id'
+              element={<EventDetailPage />}
+            />
+            <Route
+              path='*'
+              element={
+                <Navigate
+                  to='/events'
+                  replace
+                />
+              }
+            />
+          </Routes>
+        </div>
+      </div>
+    </FilterProvider>
   );
 }
