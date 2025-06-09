ab43aa79fe42f97ab7d1b6200f059c9f88cb8672
Janilee <janileesvardstal@gmail.com>
2025-06-09 13:46:37 +0100

    populating prod db

--- server/dist/index.js
+++ server/dist/index.js
@@ -8,9 +8,8 @@ const cors_1 = __importDefault(require("cors"));
 const client_1 = require("@prisma/client");
 const firebase_admin_1 = __importDefault(require("firebase-admin"));
 const dotenv_1 = __importDefault(require("dotenv"));
-const eventify_auth_d7118_firebase_adminsdk_fbsvc_fea411ca4f_json_1 = __importDefault(require("../eventify-auth-d7118-firebase-adminsdk-fbsvc-fea411ca4f.json"));
 dotenv_1.default.config();
-const serviceAccount = eventify_auth_d7118_firebase_adminsdk_fbsvc_fea411ca4f_json_1.default;
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 firebase_admin_1.default.initializeApp({
     credential: firebase_admin_1.default.credential.cert(serviceAccount)
 });
