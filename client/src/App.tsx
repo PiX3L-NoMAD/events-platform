e9f2cf02ca5bb3043385bd6b7d4b10b3c2b6bb59
Janilee <janileesvardstal@gmail.com>
2025-06-09 17:23:21 +0100

    removing duplicate FilterProvider wrapping

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,61 +8,58 @@ import EventListPage from './pages/EventListPage';
 import EventDetailPage from './pages/EventDetailPage';
 import SignIn from './components/SignIn';
 import Header from './components/Header';
-import { FilterProvider } from './contexts/FilterContext';
 import EventEditPage from './pages/EventEditPage';
 import EventCreatePage from './pages/EventCreatePage';
 
 export default function App() {
   return (
-    <FilterProvider>
-      <div className='min-h-screen bg-gray-50'>
-        <Header />
-        <div className='pt-14'>
-          <Routes>
-            <Route
-              path='/signin'
-              element={<SignIn />}
-            />
-            <Route
-              path='/'
-              element={<EventListPage />}
-            />
-            <Route
-              path='/events'
-              element={<EventListPage />}
-            />
-            <Route
-              path='/events/:id'
-              element={<EventDetailPage />}
-            />
-            <Route
-              path='/events/:id/edit'
-              element={
-                <RequireStaff>
-                  <EventEditPage />
-                </RequireStaff>
-              }
-            />
-            <Route
-              path='/events/new'
-              element={
-                <RequireStaff>
-                  <EventCreatePage />
-                </RequireStaff>
-              }
-            />
-            <Route
-              path='*'
-              element={
-                <Navigate
-                  to='/events'
-                  replace
-                />
-              }
-            />
-          </Routes>
-        </div>
+    <div className='min-h-screen bg-gray-50'>
+      <Header />
+      <div className='pt-14'>
+        <Routes>
+          <Route
+            path='/signin'
+            element={<SignIn />}
+          />
+          <Route
+            path='/'
+            element={<EventListPage />}
+          />
+          <Route
+            path='/events'
+            element={<EventListPage />}
+          />
+          <Route
+            path='/events/:id'
+            element={<EventDetailPage />}
+          />
+          <Route
+            path='/events/:id/edit'
+            element={
+              <RequireStaff>
+                <EventEditPage />
+              </RequireStaff>
+            }
+          />
+          <Route
+            path='/events/new'
+            element={
+              <RequireStaff>
+                <EventCreatePage />
+              </RequireStaff>
+            }
+          />
+          <Route
+            path='*'
+            element={
+              <Navigate
+                to='/events'
+                replace
+              />
+            }
+          />
+        </Routes>
       </div>
-    </FilterProvider>
+    </div>
   );
 }
